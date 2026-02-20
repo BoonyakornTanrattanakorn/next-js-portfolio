0503d9221c75da7dc2177ad8015daf150dc0a72a
BoonyakornTanrattankorn <boonyakorn.tan@gmail.com>
2026-02-20 17:31:34 +0700

    merge

--- component/navBar/navBar.tsx
+++ component/navBar/navBar.tsx
@@ -4,19 +4,11 @@ import '@/app/globals.css';
 
 export default function NavBar() {
   return (
-<<<<<<< HEAD
     <Navbar position="static" className="p-4 font-bold">
       {/* <NavbarBrand>
 
       </NavbarBrand> */}
       <NavbarContent justify="center" className="flex-1">
-=======
-    <Navbar position="static" className="bg-gray-800 flex p-5 gap-5 w-full justify-between">
-      {/* <NavbarBrand>
-
-      </NavbarBrand> */}
-      <NavbarContent className="bg-pink-500">
->>>>>>> 57090a76d6b9ce6cd66adebeefd1193ad0d16676
         <NavbarItem>
           <Link href="/">
             Home
@@ -33,11 +25,7 @@ export default function NavBar() {
           </Link>
         </NavbarItem>
       </NavbarContent>
-<<<<<<< HEAD
       <NavbarContent justify="end">
-=======
-      <NavbarContent className="bg-pink-500">
->>>>>>> 57090a76d6b9ce6cd66adebeefd1193ad0d16676
         <NavbarItem>
           <Link href="/contact">
             Contact
